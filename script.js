2f286c3f86492fd953c03419178b5f5583303cf4
sroy20442 <sroy20442@gmail.com>
2022-12-31 21:30:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "be41f3aad5msha074dfaae40a2c9p1964b8jsnd2b2de0b5205",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+const getWeather = (city)=>{
+	cityname.innerHTML =city
+	fetch(
+		"https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city="+city,
+		options
+	  )
+		.then((response) => response.json())
+		.then((response) => {
+		  console.log(response);
+		  cloud_pct.innerHTML = response.cloud_pct;
+		  feels_like.innerHTML = response.feels_like;
+		  humidity.innerHTML = response.humidity;
+		  max_temp.innerHTML = response.max_temp;
+		  min_temp.innerHTML = response.min_temp;
+		  sunrise.innerHTML = response.sunrise;
+		  sunset.innerHTML = response.sunset;
+		  temp.innerHTML = response.temp;
+		  wind_degrees.innerHTML = response.wind_degrees;
+		  wind_speed.innerHTML = response.wind_speed;
+		})
+		.catch((err) => console.error(err));
+}
+SubmitEvent.addEventListener("click", (e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+getWeather("delhi")
